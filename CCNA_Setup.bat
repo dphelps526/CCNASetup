0b67c2b393c34b0ced32fe1383aabf7772e2d02b
dphelps526 <119672188+dphelps526@users.noreply.github.com>
2025-02-16 09:09:48 -0500

    Initial Comment

--- CCNA_Setup.bat
+++ CCNA_Setup.bat
@@ -7,20 +7,6 @@ set INSTALL_DIR=C:\Users\%USERNAME%\Documents\CCNA
 set DESKTOP=%USERPROFILE%\Desktop
 set START_SCRIPT=%DESKTOP%\start_server.bat
 
-:: Check and install Git if missing
-where git >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Installing Git...
-    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://git-scm.com/download/win -OutFile git-installer.exe; Start-Process -Wait -FilePath .\git-installer.exe -ArgumentList '/SILENT'; del git-installer.exe}"
-)
-
-:: Check and install Python if missing
-where python >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Installing Python...
-    powershell -Command "& {[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri https://www.python.org/ftp/python/3.11.4/python-3.11.4-amd64.exe -OutFile python-installer.exe; Start-Process -Wait -FilePath .\python-installer.exe -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1'; del python-installer.exe}"
-)
-
 :: Clone the repository
 if not exist "%INSTALL_DIR%" (
     git clone %REPO_URL% "%INSTALL_DIR%"
